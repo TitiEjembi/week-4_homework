943069638a13cca49226107fce56c1ba573ecc85
Titi Ejembi <Titilope.Ejembi@gmail.com>
2025-02-10 13:11:41 +0000

    moved folders from branch to main

---
+++ question3_Lotto_Titi_Daliya.py
@@ -0,0 +1,37 @@
+import random
+# random variable generator
+
+number = 6 #number of random numbers to display
+#it ensures that there are no duplicate values in the output.
+lottery_numbers = random.sample(range(1,51),number)#using the random.sample method
+print(lottery_numbers)
+
+
+
+# for i in range(6):
+#    num =random.randint(1,50)
+#   print(num)
+# tried to create a for loop to iterate over the elements of the sequence of numbers
+# range created to have fix the numbers of attempts to  return a new object
+# used randint method on random function to generate random integer with a start and end point provided
+# numbers generated in this form was random integers which did not belong to a collection
+
+numbers = [random.randint(1,50) for i in range(6)]
+print(numbers)
+# in this method assigned the value of .randint method to  a variable called numbers
+# variable numbers has collection of numbers in the form LIST
+
+numbers_1 ={random.randint(1,50) for i in range(6)}
+print(numbers_1)
+# in this method assigned the value of .randint method to  a variable called numbers
+# variable numbers has collection of numbers in the form SETS
+
+start= 1
+end=50
+chances=6
+lotto_nums = [random.randint(start,end) for i in range(chances)]
+print(lotto_nums)
+# used the same method of randint and passed it to a new variable called lotto_nums
+# tried to automate the process to get same results for any starting
+# and ending numbers and range of numbers
+
